93c9bce67db5b6b12952403fa9674932b71bcbec
Hyunphil Choi <phil0808@gmail.com>
2017-01-19 22:06:31 -0500

    bugfix for Data2bifd

--- R/Data2bifd.R
+++ R/Data2bifd.R
@@ -17,15 +17,19 @@ Data2bifd <- function(sargvals=NULL, targvals=NULL, y=NULL,
   if (is.null(lambdas)) lambdas <- 3e-8/diff(as.numeric(range(sargvals)))
   if (is.null(lambdat)) lambdat <- 3e-8/diff(as.numeric(range(sargvals)))
 
-
   Bs <- eval.basis(sargvals,sbasisobj)
   Bt <- eval.basis(targvals,tbasisobj)
 
   if (!is.null(Lfdobjs)) Ps <- getbasispenalty(sbasisobj,Lfdobj=Lfdobjs) else Ps <- 0
-  if (!is.null(Lfdobjt)) Pt <- getbasispenalty(sbasisobj,Lfdobj=Lfdobjt) else Pt <- 0
+  if (!is.null(Lfdobjt)) Pt <- getbasispenalty(tbasisobj,Lfdobj=Lfdobjt) else Pt <- 0
 
   coef <- t(chol2inv(chol(crossprod(Bt) + lambdat * Pt)) %*%
             t(Bt) %*% t(y) %*% Bs %*%
             chol2inv(chol(crossprod(Bs) + lambdas * Ps)))
-  return(bifd(coef,sbasisobj=sbasisobj,tbasisobj=tbasisobj))
+  bifdobj <- bifd(coef,sbasisobj=sbasisobj,tbasisobj=tbasisobj)
+  bifdobj$Q <- sum(diag(crossprod(y - Bs %*% coef %*% t(Bt) ))) +
+               lambdat*sum(diag(Bt%*%t(coef)%*%Ps%*%coef%*%t(Bt))) +
+               lambdas*sum(diag(Bs%*%coef%*%Pt%*%t(coef)%*%t(Bs))) +
+               lambdat*lambdas*sum(diag(t(coef)%*%Ps%*%coef%*%Pt))
+  return(bifdobj)
 }
